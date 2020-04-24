a4edbd4ae252244454570008c32365038622e37f
krzysiekkulig <Krzysiek.kulig@gmail.com>
2020-04-24 14:27:32 +0200

    App Done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,94 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+// const testData = [
+//   { name: "Krzysiek Kulig", avatar_url: "https://avatars1.githubusercontent.com/u/19195251?v=4", company: "Targetly" },
+//   { name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu" },
+//   { name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook" },
+// ];
+
+const CardList = (props) => (
+  <div>
+    <div className="row">
+      {props.profiles.map(profile => <div className="col"><Card key={profile.id} {...profile} /></div>)}
     </div>
-  );
+  </div>
+
+)
+
+class Form extends React.Component {
+  state = {userName: ''}
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    const resp = await axios.get(`https://api.github.com/users/${this.state.userName}`)
+    this.props.onSubmit(resp.data);
+    this.setState({userName: ''})
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div className="row">
+          <div className="col-md-4">
+            <div className="input-group">
+              <input 
+              type="text" 
+              className="form-control" 
+              placeholder="Github username"
+              onChange={ event => this.setState({userName: event.target.value})}
+              value = {this.state.userName} 
+              required 
+              />
+              <div className="input-group-append">
+                <button className="btn btn-secondary">Add Card</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+    )
+  }
+}
+
+class Card extends React.Component {
+  render() {
+    const profile = this.props;
+    return (
+      <div>
+        <div className="card" style={{ width: "8rem" }}>
+          <img className="card-img-top" src={profile.avatar_url} alt="Card image cap"></img>
+          <div className="card-body">
+            <h6 className="card-title">{profile.name}</h6>
+            <p className="card-text">{profile.company}</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+class App extends React.Component {
+
+  state = {
+    profiles: [],
+  }
+  addNewProfile = (profileData) => {
+    this.setState(prevState => ({
+      profiles: [...prevState.profiles, profileData]
+    }));
+  }
+  render() {
+    return (
+      <div className="container">
+        <h5>{this.props.title}</h5>
+        <Form onSubmit={this.addNewProfile} />
+        <CardList profiles={this.state.profiles} />
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App title = "elo elo" />
   </React.StrictMode>,
   document.getElementById('root')
 );
